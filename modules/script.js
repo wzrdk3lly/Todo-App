df0bb78033543d546768ffadc62f5f2eb61a5264
wzrdk3lly <dkmk1149@gmail.com>
2022-09-29 16:08:19 -0400

    ADD ability to pass todo submission to javascript

--- modules/script.js
+++ modules/script.js
@@ -10,15 +10,15 @@ let addTodobtn = document.querySelector(".btn-add-todo");
 let rendorSection = document.querySelector(".render");
 
 // Render todo box when the add todo button is clicked
-// TODO add the button back upon submission
 addTodobtn.addEventListener("click", () => {
+  addTodobtn.classList.add("invisible");
   rendorSection.innerHTML = `
   <!-- Render add todo text box -->
   <div class="ml-96">
-      <div class=" block p-6 rounded-lg shadow-lg ml-24 mt-2 bg-white max-w-md">
-          <form>
+      <div class=" block p-6 rounded-lg shadow-lg ml-24 mt-2 bg-emerald-300 max-w-md">
+          <form id="todo">
             <div class="form-group mb-6">
-              <input type="text" class="form-control block
+              <input id="todo-title" type="text" class="form-control block
                 w-full
                 px-3
                 py-1.5
@@ -35,7 +35,7 @@ addTodobtn.addEventListener("click", () => {
                 placeholder="Title">
             </div>
             <div class="form-group mb-6">
-              <input type="date" class="form-control block
+              <input id="todo-date" type="date" class="form-control block
                 w-full
                 px-3
                 py-1.5
@@ -53,6 +53,7 @@ addTodobtn.addEventListener("click", () => {
             </div>
             <div class="form-group mb-6">
               <textarea
+              id="todo-description"
               class="
                 form-control
                 block
@@ -82,7 +83,7 @@ addTodobtn.addEventListener("click", () => {
               w-full
               px-6
               py-2.5
-              bg-blue-600
+              bg-cyan-500
               text-white
               font-medium
               text-xs
@@ -90,13 +91,37 @@ addTodobtn.addEventListener("click", () => {
               uppercase
               rounded
               shadow-md
-              hover:bg-blue-700 hover:shadow-lg
-              focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-              active:bg-blue-800 active:shadow-lg
+              hover:bg-cyan-600 hover:shadow-lg
+              focus:bg-cyan-600 focus:shadow-lg focus:outline-none focus:ring-0
+              active:bg-cyan-600 active:shadow-lg
               transition
               duration-150
               ease-in-out">Add Todo</button>
           </form>
         </div>
       </div>`;
+
+  let form = document.getElementById("todo");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let title = document.getElementById("todo-title").value;
+    let date = document.getElementById("todo-date").value;
+    let description = document.getElementById("todo-description").value;
+    console.log("The todo details are ", title, date, description);
+
+    // addBookToLibrary(title, author, isRead);
+
+    //clear form after submission
+    form.reset();
+
+    //remove form from html - create a reset function for refactoring  s
+    rendorSection.innerHTML = ``;
+    addTodobtn.classList.remove("invisible");
+  });
 });
+
+// style the card
+//TODO: create card so that the todo details can be populated on the card and displayed to the screen
+// There would need to be some sort of display function that iterates trough the todos and list them.
